74c583a83c1d3ab3233460b9bc89556e3c7e5d86
postolenko <denis.postolenko@gmail.com>
2020-08-20 17:26:17 +0300

    Thalosso

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # http://postolenko.github.io/bestflat24/index_2.html
 # http://postolenko.github.io/bestflat24/questions.html
 # http://postolenko.github.io/bestflat24/apartments.html
+# http://postolenko.github.io/bestflat24/contacts.html
--- js/map.js
+++ js/map.js
@@ -1,19 +1,42 @@
 ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-        center: [55.755814, 37.617635],
-        zoom: 14
-    }, {
-        searchControlProvider: 'yandex#search'
-    });
+    if($("#map").length > 0) {
+        var myMap = new ymaps.Map('map', {
+            center: [55.755814, 37.617635],
+            zoom: 14
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+        myPlacemark1 = new ymaps.Placemark([55.755814, 37.617635], {
+            hintContent: ''
+        }, {
+            // iconLayout: 'default#imageWithContent',
+            // iconImageHref: 'img/yellow_marker.png',
+            // iconImageSize: [39, 35],
+            // iconImageOffset: [19, -17]
+        });
+        myMap.geoObjects.add(myPlacemark1);
+    }
+// });
 
-    myPlacemark1 = new ymaps.Placemark([55.755814, 37.617635], {
-        hintContent: ''
-    }, {
-        // iconLayout: 'default#imageWithContent',
-        // iconImageHref: 'img/yellow_marker.png',
-        // iconImageSize: [39, 35],
-        // iconImageOffset: [19, -17]
-    });
+// ymaps.ready(function () {
 
-    myMap.geoObjects.add(myPlacemark1);
+    if($("#map_2").length > 0) {
+        var myMap2 = new ymaps.Map('map_2', {
+            center: [55.755814, 37.617635],
+            zoom: 14
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+
+        myPlacemark3 = new ymaps.Placemark([55.755814, 37.617635], {
+            hintContent: ''
+        }, {
+            // iconLayout: 'default#imageWithContent',
+            // iconImageHref: 'img/yellow_marker.png',
+            // iconImageSize: [39, 35],
+            // iconImageOffset: [19, -17]
+        });
+
+        myMap2.geoObjects.add(myPlacemark3);
+    }
 });
